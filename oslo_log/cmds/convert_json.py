6ead16f9ec085b8c3becc7556981754266442578
Kevin Rasmussen <krasmussen@godaddy.com>
2017-01-20 14:57:10 -0700

    Added tail esk support, filtering off logger/loglevel, and PEP8 compliance

--- oslo_log/cmds/convert_json.py
+++ oslo_log/cmds/convert_json.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
 #    a copy of the License at
@@ -16,14 +17,15 @@ import argparse
 import collections
 import functools
 import sys
+import time
 
 from oslo_serialization import jsonutils
 from oslo_utils import importutils
 import six
 
-termcolor = importutils.try_import('termcolor')
-
 from oslo_log import log
+
+termcolor = importutils.try_import('termcolor')
 
 
 _USE_COLOR = False
@@ -33,32 +35,66 @@ def main():
     global _USE_COLOR
     args = parse_args()
     _USE_COLOR = args.color
-    formatter = functools.partial(console_format, args.prefix, args.locator)
-    for line in reformat_json(args.file, formatter):
-        print(line)
+    formatter = functools.partial(
+                                  console_format,
+                                  args.prefix,
+                                  args.locator,
+                                  loggers=args.loggers,
+                                  levels=args.levels,
+                                 )
+    if args.lines:
+        # Read backward until we find all of our newline characters
+        # or reach the beginning of the file
+        args.file.seek(0, 2)
+        newlines = 0
+        pos = args.file.tell()
+        while newlines <= args.lines and pos > 0:
+            pos = pos - 1
+            args.file.seek(pos)
+            if args.file.read(1) == '\n':
+                newlines = newlines + 1
+    try:
+        for line in reformat_json(args.file, formatter, args.follow):
+            print(line)
+    except KeyboardInterrupt:
+        sys.exit(0)
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("file",
+    parser.add_argument('file',
                         nargs='?', default=sys.stdin,
                         type=argparse.FileType(),
-                        help="JSON log file to read from (if not provided"
-                             " standard input is used instead)")
-    parser.add_argument("--prefix",
+                        help='JSON log file to read from (if not provided'
+                             ' standard input is used instead)')
+    parser.add_argument('--prefix',
                         default='%(asctime)s.%(msecs)03d'
                                 ' %(process)s %(levelname)s %(name)s',
-                        help="Message prefixes")
-    parser.add_argument("--locator",
+                        help='Message prefixes')
+    parser.add_argument('--locator',
                         default='[%(funcname)s %(pathname)s:%(lineno)s]',
-                        help="Locator to append to DEBUG records")
-    parser.add_argument("-c", "--color",
+                        help='Locator to append to DEBUG records')
+    parser.add_argument('-c', '--color',
                         action='store_true', default=False,
-                        help="Color log levels (requires `termcolor`)")
+                        help='Color log levels (requires `termcolor`)')
+    parser.add_argument('-f', '--follow',
+                        action='store_true', default=False,
+                        help='Continue parsing new data until'
+                             ' KeyboardInterrupt')
+    parser.add_argument('-n', '--lines',
+                        required=False, type=int,
+                        help='Last N number of records to view.'
+                             ' (Not Compatable with --loggers or --levels)')
+    parser.add_argument('--loggers',
+                        nargs='*', default=[],
+                        help='only return results matching given logger(s)')
+    parser.add_argument('--levels',
+                        nargs='*', default=[],
+                        help='Only return lines matching given log level(s)')
     args = parser.parse_args()
     if args.color and not termcolor:
-        raise ImportError("Coloring requested but `termcolor` is not"
-                          " importable")
+        raise ImportError('Coloring requested but `termcolor` is not'
+                          ' importable')
     return args
 
 
@@ -82,32 +118,44 @@ def colorise(key, text=None):
 
 
 def warn(prefix, msg):
-    return "%s: %s" % (colorise('exc', prefix), msg)
+    return '%s: %s' % (colorise('exc', prefix), msg)
 
 
-def reformat_json(fh, formatter):
+def reformat_json(fh, formatter, follow=False):
     # using readline allows interactive stdin to respond to every line
     while True:
         line = fh.readline()
         if not line:
-            break
+            if follow:
+                time.sleep(0.1)
+                continue
+            else:
+                break
         line = line.strip()
         if not line:
             continue
         try:
             record = jsonutils.loads(line)
         except ValueError:
-            yield warn("Not JSON", line)
+            yield warn('Not JSON', line)
             continue
         for out_line in formatter(record):
             yield out_line
 
 
-def console_format(prefix, locator, record):
+def console_format(prefix, locator, record, loggers=[], levels=[]):
     # Provide an empty string to format-specifiers the record is
     # missing, instead of failing. Doesn't work for non-string
     # specifiers.
     record = collections.defaultdict(str, record)
+    # skip if the record doesn't match a logger we are looking at
+    if len(loggers) > 0:
+        name = record.get('name')
+        if name not in loggers:
+            return
+    if len(levels) > 0:
+        if record.get('levelname') not in levels:
+            return
     levelname = record.get('levelname')
     if levelname:
         record['levelname'] = colorise(levelname)
@@ -130,7 +178,7 @@ def console_format(prefix, locator, record):
 
     tb = record.get('traceback')
     if tb:
-        for tb_line in tb.splitlines():
+        for tb_line in tb:
             yield ' '.join([prefix, tb_line])
 
 
